846c30f2c27ee32aa2e94ef517358cb2b7ba6251
ashish.sawant-ext@rexel.com <c2Wr4w2H>
2020-07-06 22:14:46 +0530

    Building a Nested Component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,15 @@ import { NgModule } from '@angular/core';
 
 import { EventsAppComponent } from './eventsapp.component';
 import { EventsListComponent } from './events/eventslist.component';
+import { EventModeComponent } from './events/eventmode.component';
 import { FormsModule } from '@angular/forms';
 
 
 @NgModule({
   declarations: [
     EventsAppComponent,
-    EventsListComponent
+    EventsListComponent,
+    EventModeComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/events/eventmode.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'event-mode',
+  templateUrl:'./eventmode.component.html',
+  styles:[`
+  	.event-mode-img {height: 50px;width: 50px;margin-bottom: -18px;}
+  `] 
+})
+
+export class EventModeComponent {
+	
+}
