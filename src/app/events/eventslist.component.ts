7b3c07e897df38e9c0d8df2f57a7bd5f50fdf1e7
ashish.sawant-ext@rexel.com <c2Wr4w2H>
2020-07-06 17:00:39 +0530

    Property Binding

--- src/app/events/eventslist.component.ts
+++ src/app/events/eventslist.component.ts
@@ -6,14 +6,18 @@ import { Component } from '@angular/core';
   styles:[`
   	.ml10 {margin-left:10px;}
   	.events-text {font-family: Georgia;font-size: 15pt;}
+  	.event-logo {height: 50px;width: 50px;margin-bottom: -18px;}
+  	.regSoFar {color:#f0ab00}
   `] 
 })
 
 
 export class EventsListComponent {
+	eventLogoImgUrl: string = '/assets/images/event-logo.png';
 	event = {
 		srno:1,
 		name:"Event X",
-		date:"12-Dec-2002"
+		date:"12-Dec-2002",
+		totalRegistrationSoFar:Math.floor((Math.random() * 100) + 1)
 	}
 }
