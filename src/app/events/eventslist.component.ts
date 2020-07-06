d1d5a9cf772b277cd75cd791a147d0dd839c98d5
ashish.sawant-ext@rexel.com <c2Wr4w2H>
2020-07-06 14:08:57 +0530

    Using External Templates

--- src/app/events/eventslist.component.ts
+++ src/app/events/eventslist.component.ts
@@ -2,11 +2,7 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'events-list',
-  template: `
-  			 <h2>List of Events</h2>
-  			 <hr>
-  			 <b>{{event.name}} </b> on {{event.date}}
-  			`
+  templateUrl:'./eventslist.component.html' 
 })
 
 
